db448d5e3c650bfe535639ffc524394f0b856807
SMegalo <lang.jiang@ximalaya.com>
2022-06-06 17:04:52 +0800

    feat: 更新模板

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,25 +1,19 @@
-import React, { FC } from 'react';
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/lib/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-import XLayout from '@/pages/Xlayout';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'mobx-react';
-import { AppStore } from '@/stores/AppStore';
+import React, { FC } from "react";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import XLayout from "@/pages/Xlayout";
+import { BrowserRouter as Router } from "react-router-dom";
 
-moment.locale('zh-cn');
-
-const app = AppStore.create({});
+moment.locale("zh-cn");
 
 const App: FC = () => {
   return (
     <ConfigProvider locale={zhCN}>
-      <Provider app={app}>
-        <Router basename="/gatekeeper/supply-chain-manage">
-          <XLayout />
-        </Router>
-      </Provider>
+      <Router basename="/gatekeeper/supply-chain-manage">
+        <XLayout />
+      </Router>
     </ConfigProvider>
   );
 };
